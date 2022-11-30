07759fa9d520003bb08b101852a65f36909040dd
F0rry <fred.grebenkin@gmail.com>
2022-11-30 01:09:18 +0100

    Init application

---
+++ Source.cpp
@@ -0,0 +1,94 @@
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+void checkValidInput()
+{
+	if (cin.fail())
+	{
+		throw "Incorrect input";
+	}
+}
+void checkValidParams(int n)
+{
+	if (n < 4)
+	{
+		throw "Input correct data";
+	}
+}
+int calculate(int n, int x)
+{
+	
+	double y = 0;
+	if (n < 2)
+	{
+		cout << " Ошибка ";
+		return 0;
+	}
+	else
+	
+		if (x < 0)
+		{
+			for (int i = 1; i <= n - 1; i++)
+			{
+				for (int j = 1; j < 1; j++)
+					y += j / (j * j + j);
+			}
+		}
+		else
+			for (int i = 1; i <= n - 1; i++)
+				y = x - i;
+	
+	
+	return 0;
+}
+
+int main() 
+{
+	setlocale(LC_ALL, "RUS");
+	double x, n, j, p, u;
+	double y = 0;
+	try
+	{
+	cout << "Введите n>=2, n=";
+	cin >> n;
+	checkValidInput();
+	checkValidParams(n);
+	cout << "Введите x= ";
+	cin >> x;
+	checkValidInput();
+	if (n < 2)
+	{
+		cout << " Ошибка ";
+		return 0;
+	}
+		else
+		
+	if (x < 0)
+	{
+		for (int i = 1; i <= n - 1; i++) 
+		{
+			for (int j = 1; j < 1; j++)
+				y += j / (j * j + j);	
+		}
+	}
+	else
+		for (int i = 1; i <= n - 1; i++)
+			y = x - i;
+	cout << "x= " << x << "; " << "y= " << calculate(n,x) << endl;
+	x += p; 
+	return 0;
+	}
+	catch (const char* ex)
+	{
+		cout << ex << endl;
+		return -1;
+	}
+	catch (...)
+	{
+		cout << "Неизвестная ошибка" << endl;
+		return -2;
+	}
+		
+}
---
+++ UnitTest1.cpp
@@ -0,0 +1,62 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "C:\Users\fredg\OneDrive\Рабочий стол\ipz2\ipz2\Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+
+{
+	TEST_CLASS(calcuate_Tests)
+	{public:
+		TEST_METHOD(calculate_get4and7_14returned) {
+			int n = 4;
+			int x = 7;
+			int expected = 0;
+			int actual = calculate(n, x);
+			Assert::AreEqual(expected, actual);
+		}
+	public:
+		TEST_METHOD(calculate_get15and6_170returned)
+		{
+			int n = 15;
+			int x = 6;
+			int expected = 0;
+			int actual = calculate(n, x);
+			Assert::AreEqual(expected, actual);
+		}
+	};
+	TEST_CLASS(checkValidParams_Tests)
+	{
+	public:
+
+		TEST_METHOD(checkValidParams_get10_exceptionNotThrown)
+		{
+			int n = 10;
+			try
+			{
+				checkValidParams(n);
+				Assert::IsTrue(true);
+			}
+			catch (...)
+			{
+				Assert::Fail();
+
+			}
+
+		}
+	public:
+		TEST_METHOD(checkValidParams_get3_exceptionNotThrown)
+		{
+			int n = 3;
+			try {
+				checkValidParams(n);
+				Assert::Fail();
+			}
+			catch (...)
+			{
+				Assert::IsTrue(true);
+			}
+		}
+	};
+}
